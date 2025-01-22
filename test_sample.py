811ab315188f3fc38d6f995e99eeb7da9e9d9f89
NeelGhoshal <neelghoshal02@gmail.com>
2025-01-22 01:17:34 -0500

    Adding failing test case

--- test_sample.py
+++ test_sample.py
@@ -10,3 +10,6 @@ def test_factorial_2():
 def test_factorial_3():
     with pytest.raises(ValueError, match="Factorial is defined only for positive integers."):
         factorial(-5)
+
+def test_factorial_4():
+    assert factorial(5) == 100 #factorial(5) is 120, not 100
